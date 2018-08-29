2b60e46ec53a749215ead0ebca0eb29c593df402
josue cabrera <ultimatejosue9@gmail.com>
2018-08-28 19:34:43 -0600

    Primera Fase del Lab0

---
+++ js/controladorWeb.js
@@ -0,0 +1,18 @@
+
+
+/*Controladores del index*/
+app.controller("ControladorIndex", function($scope) {
+    $scope.Info = "Página Web dedicada a los amantes del fútbol, pero sobre todo a aquellos que juegan bajo los tres palos y saben lo importante de la labor de un portero.";
+    $scope.Mision = "Somos un movimiento adherido a una parroquia que nos dedicamos a fortalecer nuestra fe ayudando y predicando a aquellas personas que lo requieran.";
+    $scope.VidaComuni = "La vida en comunidad es parte importante de nuestro carisma y formación, por eso, mas que miembros de un grupo, somos amigos que integran una comunidad.";
+    $scope.Proyectos = "Nuestros proyectos como jóvenes Dominicos en la ciudad consisten en el apoyo a la juventud de nuestra parroquia y la transformación de nuestra zona pastoral, a través de pequeños acto de amor ayudando a los que necesiten de nosotros.";
+});
+
+/*Controlador Nosotros*/
+app.controller("ControladorNosotros", function($scope) {
+    $scope.Conocenos = "Era el año 2003 y un grupo de amigso que recién había recibido el sacramento de la confirmación, deciden iniciar un grupo de jóvenes que, pueda continuar su formación cristiana, rosolver sus dudas, aclarar inquietudes y crecer como personas. Así nace el Movimiento Juvenil Domincano Santo Tomás de Aquino.";
+    $scope.Conocenos2 = "La esencia del movimiento se mantiene viviendo la fe según los 4 pilares de nuestra orden: Oración, Vida en comunidad, Estudio y Predicación. Es así como hemos planateado los distintos campos de misión, siendo siempre lo más importante el predicar a los jóvenes sobre el amor de Dios y su hijo, desde nuestra propia experiencia y aprendizaje.";
+    $scope.Conocenos3 = "Predicamos desde la idea de que la mejor forma de evangelizar un joven es otro joven predicando, capaz de mostrar la belleza de ser católico, la alegría de ser dominico; conscientes de que hemos de estar en constante búsqueda para seguir creciendo, predicando y aprendiendo. Somos un movimiento adherido a la parroquia Santo Tomás de Aquino Inmaculada de Tívoli que trabaja con la juventud y la Familia Dominicacana para fortalecer nuestra fe de tal manera que nos hes posible ayudar a las personas que se encuentran a nuestro alcance y requieran nuestra ayuda. Además buscamos expandirnos para acoger a jóvenes que sientan la necesidad de conocer y compartir más sobre Dios y el carisma domnicano.";
+    $scope.NuestraMision = "Nuestra misión abraca muchos campos en los cuales siempre velamos por impregnar nuestra juventud y esencia domincana. Porque no basta reunirse hablar de Dios sino actuar en base a nuestros ideales domincanos así como nuestros ideales como personas para que la sociedad en la que vivimos cresca a una mejor. Participamos activamente en la dínamica parroquial de la iglesia dominicana Santo Tomás de Aquino Inmaculada de Tívoli. Estas son: ";
+    $scope.NuestraMision2 = "Empezamos a trabajar en un Proyecto Humanitario con los propios miembros dentro del área metropolitana donde realizamos bolsas de comida a las personas de la calle con lo cual tratamos de sacarle una sonrisa a esas personas. Además, nos encontramos trabjando en un proyecto de ayuda al indigente que consiste en brindarles un tiempo de comida con lo que fortalecemos nuestra conciencia para hacer de una mejor sociedad guatemalteca."
+});
