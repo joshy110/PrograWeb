82d45d794fd83be1f83299bf967df1a41930e096
josue cabrera <ultimatejosue9@gmail.com>
2018-08-30 17:25:29 -0600

    5 botones agregados y funcionan correctamente. A su vez se modificaron algunos estilos utilizados.

--- js/controladorFunciones.js
+++ js/controladorFunciones.js
@@ -2,14 +2,15 @@ $('#btn_azul').click(function()
 {
 	$('#Mision').css('background-color','#123DE0');
 	$('#Mision').css('color', 'white');
+	$('#Mision').css('font-family', 'Comic Sans MS');
+
 });
 
 $('#btn_verde').click(function()
 {
-	var idM = $('#Mision');
-	console.log(idM);
 	$('#Vision').css('background-color','#5EC809');
 	$('#Vision').css('color', 'white');
+	$('#Vision').css('font-family', 'Arial Black');
 });
 
 $('#btn_cambioClase').click(function()
@@ -23,6 +24,16 @@ $('#btn_regresarClase').click(function()
 	$('.TituloscolumnasPrincipal').css('background-color','#E5D808');
 	$('.TituloscolumnasPrincipal').css('color', 'white');
 	$('.TituloscolumnasPrincipal').css('font-family', 'Georgia');
+});
+$('#btn_regresarParrafo').click(function()
+{
+	$('#Mision').css('background-color','#000203');
+	$('#Mision').css('color', '#fff');
+	$('#Mision').css('font-family', 'Georgia');
+
+	$('#Vision').css('background-color','#000203');
+	$('#Vision').css('color', '#fff');
+	$('#Vision').css('font-family', 'Georgia');
 });
 
 
