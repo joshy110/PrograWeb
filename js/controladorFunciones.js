7a297037091256606fb436a38551fd2c00cdd9b3
josue cabrera <ultimatejosue9@gmail.com>
2018-08-29 19:06:32 -0600

    4 botones agregados y realizan sus funciones de forma correcta

--- js/controladorFunciones.js
+++ js/controladorFunciones.js
@@ -11,3 +11,19 @@ $('#btn_verde').click(function()
 	$('#Vision').css('background-color','#5EC809');
 	$('#Vision').css('color', 'white');
 });
+
+$('#btn_cambioClase').click(function()
+{
+	$('.TituloscolumnasPrincipal').css('background-color','#080302');
+	$('.TituloscolumnasPrincipal').css('font-family', 'Arial');
+});
+
+$('#btn_regresarClase').click(function()
+{
+	$('.TituloscolumnasPrincipal').css('background-color','#E5D808');
+	$('.TituloscolumnasPrincipal').css('color', 'white');
+	$('.TituloscolumnasPrincipal').css('font-family', 'Georgia');
+});
+
+
+
