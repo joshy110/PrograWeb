e940c58b79436f4295c2f2ab43124a558e7fb603
Josue David Cabrera <jcabrera@domcoin.net>
2018-09-07 16:02:12 -0600

    nuevos cambios

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 
 import "css/estilos.css";
 
-const name = "Joshy";
-
 function formatName(user) {
   return user.firstName + " " + user.lastName;
 }
@@ -13,8 +11,6 @@ const user = {
   firstName: "Joshy",
   lastName: "Cabrera"
 };
-
-const element = <h1>Hello, {formatName(user)}!</h1>;
 
 function App() {
   return (
