98c939b8817bcd196c2b02fee00fd7e67c89c487
Josue David Cabrera <jcabrera@domcoin.net>
2018-09-07 12:15:56 -0600

    Preparativos para FrmaeWorlReact

---
+++ index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "css/estilos.css";
+
+const name = "Joshy";
+
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+const user = {
+  firstName: "Joshy",
+  lastName: "Cabrera"
+};
+
+const element = <h1>Hello, {formatName(user)}!</h1>;
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello {formatName(user)}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
